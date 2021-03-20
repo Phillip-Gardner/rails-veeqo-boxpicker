558dccaad13b05655236af5ea3849c69241ae206
Phillip Gardner <pbg.gardner@gmail.com>
2021-03-20 20:36:22 +0000

    trying to figure it out

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -3,6 +3,32 @@ class BoxesController < ApplicationController
     @orders = Veeqo::Order.all
   end
 
+  # def find_the_box
+  # @orders.each do |order|
+  #   items = []
+  #   # x = order.line_items[:quantity]
+  #   # x.times do
+  #   order.line_items.each do |item|
+  #   a = item[:sellable][:measurement_attributes][:height]
+  #   b = item[:sellable][:measurement_attributes][:width]
+  #   c = item[:sellable][:measurement_attributes][:depth]
+  #   items << {dimensions => [a,b,c]}
+  #   end
+  #   container = EasyBoxPacker.find_smallest_container( items )
+  #   @container = container
+  # end
+
+  # container = EasyBoxPacker.find_smallest_container(
+  #   items: [
+  #   { dimensions: [2, 3, 5], weight: 47 },
+  #   { dimensions: [2, 3, 5], weight: 47 },
+  #   { dimensions: [3, 3, 1], weight: 24 },
+  #   { dimensions: [1, 1, 4], weight: 7 },
+  # ]
+  # )
+
+  # container # [10, 10, 10]
+
   # private
 
   # # def measurements
