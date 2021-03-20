8937cde03081dacefcd8b7ad30a29b89e10fdcbe
nicolas vassel <nicolas.vassel@gmail.com>
2021-03-20 12:43:58 +0000

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning database"
+
+Box.destroy_all
+
+puts "creating boxes"
+
+require 'csv'
+boxes     = []
+csv_options = { headers: :first_row, header_converters: :symbol }
+CSV.foreach("#{Rails.root}/lib/seeds/type_of_pack.csv", csv_options) do |row|
+  row[:package] = row[:package]
+  row[:length] = row[:length].to_f
+  row[:width] = row[:width].to_f
+  row[:height] = row[:height].to_f
+  boxes << Box.create(row.to_h)
+end
+
+p boxes
